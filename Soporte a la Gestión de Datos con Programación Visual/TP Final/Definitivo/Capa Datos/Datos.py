17d1c908bbf3b92ee6067ba85773759bee56b1e9
andresjbotello <44442474+andresjbotello@users.noreply.github.com>
2019-10-07 21:24:35 -0300

    Create Datos.py

---
+++ Soporte a la Gestión de Datos con Programación Visual/TP Final/Definitivo/Capa Datos/Datos.py
@@ -0,0 +1,28 @@
+
+def Guarda_Audio(nombre, autor, descripcion, frames):
+    """Guarda audio original en la base de datos"""
+    pass
+
+def Guarda_Analisis(frames, chroma, id_audio):
+    """Guarda analisis de audio en la base de datos"""
+    pass
+
+def Get_All_Audios():
+    """Retorna todos los audios de la base de datos"""
+    pass
+
+def Get_One_Audio(id_audio):
+    """Retorna un audio en particular de la base de datos"""
+    pass
+
+def Modifica_Audio(id_audio, nombre, descripcion):
+    """Modifica el nombre o la descripcion de un audio"""
+    pass
+
+def Elimina_Audio(id_audio):
+    """Elimina un audio y su analisis"""
+    pass
+
+def Get_One_Analisis(id_audio):
+    """Retorna el analisis de un audio"""
+    pass
